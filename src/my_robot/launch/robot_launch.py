9ef2ef317a3ae6b434986cd761dd1c6ada2fd97e
ngocrc <tienngoc2003dh@gmail.com>
2024-11-13 22:03:31 +0700

    add twist_mux.yaml, new urdf rviz config file

--- src/my_robot/launch/robot_launch.py
+++ src/my_robot/launch/robot_launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
 
     rviz_arg = DeclareLaunchArgument(
         name='rvizconfig',
-        default_value=os.path.join(pkg_path,'config','urdf.rviz'),
+        default_value=os.path.join(pkg_path,'config','urdf2.rviz'),
         description='Absolute path to rviz config file')
 
     rviz2 = Node(
