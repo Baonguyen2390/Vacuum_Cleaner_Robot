5982fe121289a3ba599b065349987f103b2201e6
ngocrc <tienngoc2003dh@gamail.com>
2025-07-08 13:19:57 +0700

    add comments for launching robot

--- launch.sh
+++ launch.sh
@@ -2,5 +2,8 @@
 
 echo "Script executed from: ${PWD}"
 
+# have to source map converter and opennav first
 gnome-terminal --tab --title="robot" --command="bash -c 'cd ${PWD} && source install/setup.bash && ros2 launch my_robot robot_launch.py'"
+
+# must not source opennav
 gnome-terminal --tab --title="nav2" --command="bash -c 'cd ${PWD} && source install/setup.bash && ros2 launch my_robot navigation_launch.py use_sim_time:=false'"
