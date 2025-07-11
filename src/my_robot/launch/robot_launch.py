12d3333c1a6b9b362bb332ed2f0da86528657d0b
ngocrc <tienngoc2003dh@gamail.com>
2025-07-11 16:43:06 +0700

    remapping odom for real robot

--- src/my_robot/launch/robot_launch.py
+++ src/my_robot/launch/robot_launch.py
@@ -57,7 +57,7 @@ def generate_launch_description():
         output="both",
         remappings=[
             ("~/robot_description", "/robot_description"),
-            ("/diffbot_base_controller/cmd_vel", "/cmd_vel"),
+            ("/diffbot_base_controller/odom", "/odom"),
         ],
     )
     robot_state_pub_node = Node(
