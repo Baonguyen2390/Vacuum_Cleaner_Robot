e70a68c9367984f83d105f2e84f3414eb495c2aa
ngocrc <tienngoc2003dh@gmail.com>
2025-10-11 17:49:27 +0700

    move common launch entities into bringup_launch

---
+++ src/my_robot/launch/bringup_launch.py
@@ -0,0 +1,110 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    my_robot_dir = get_package_share_directory('my_robot')
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+    'use_sim_time',
+    default_value='False',
+    description='Use simulation (Gazebo) clock if true, also used to determine simulated or real robot is launched')
+
+    # Use xacro to process the description file
+    xacro_file_dir = PathJoinSubstitution([
+        my_robot_dir,
+        'description',
+        PythonExpression(["'robot_simulation.xacro' if ", use_sim_time, " else 'robot.xacro'"]),
+    ])
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]),
+        " ",
+        xacro_file_dir,
+    ])
+
+    # Configure the nodes
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description_content,
+            'use_sim_time': use_sim_time,
+        }]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diffbot_base_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    twist_mux_params_dir = os.path.join(my_robot_dir, "config", "twist_mux.yaml")
+    twist_mux = Node(
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params_dir],
+        remappings=[('/cmd_vel_out','/diffbot_base_controller/cmd_vel_unstamped')],
+    )
+
+    ros2_mapping = IncludeLaunchDescription(
+        PathJoinSubstitution([
+            FindPackageShare('ros2_mapping'),
+            'launch',
+            'online_async_launch.py'
+        ]),
+        launch_arguments={
+            'use_sim_time' : use_sim_time,
+        }.items()
+    )
+
+    robot_simulation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([FindPackageShare('my_robot'), 'launch', 'robot_simulation_launch.py'])
+        ]),
+        condition=IfCondition(use_sim_time),
+    )
+
+    # Delay start of joint_state_broadcaster after `robot_controller`
+    # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
+    delayed_actions = [
+        joint_state_broadcaster_spawner,
+        ros2_mapping,
+    ]
+    delayed_actions_handler = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=delayed_actions,
+        )
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_cmd)
+
+    ld.add_action(robot_simulation_launch)
+
+    ld.add_action(node_robot_state_publisher)
+    ld.add_action(twist_mux)
+    ld.add_action(robot_controller_spawner)
+    ld.add_action(delayed_actions_handler)
+
+    return ld
