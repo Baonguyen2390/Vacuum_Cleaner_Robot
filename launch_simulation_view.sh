ac693123250bb43c63b67aff5159e00543125b30
ngocrc <tienngoc2003dh@gmail.com>
2025-10-16 01:10:55 +0700

    add use_sim_time into view nodes

--- launch_simulation_view.sh
+++ launch_simulation_view.sh
@@ -2,5 +2,5 @@
 
 echo "Script executed from: ${PWD}"
 
-gnome-terminal --tab --title="rviz" --command="bash -c 'cd ${PWD} && source install/setup.bash && ros2 launch my_robot robot_simulation_view_launch.py'"
-gnome-terminal --tab --title="joy" --command="bash -c 'ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/cmd_vel_joy'"
+gnome-terminal --tab --title="rviz" --command="bash -c 'cd ${PWD} && source install/setup.bash && ros2 launch my_robot robot_view_launch.py use_sim_time:=true'"
+gnome-terminal --tab --title="joy" --command="bash -c 'ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/cmd_vel_joy -p use_sim_time:=true'"
--- src/my_robot/launch/robot_view_launch.py
+++ src/my_robot/launch/robot_view_launch.py
@@ -10,6 +10,13 @@ def generate_launch_description():
     # Specify the name of the package and path to xacro file within the package
     my_robot_dir = os.path.join(get_package_share_directory('my_robot'))
 
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+    'use_sim_time',
+    default_value='False',
+    description='Use simulation (Gazebo) clock if true, also used to determine simulated or real robot is launched')
+
     rviz_arg = DeclareLaunchArgument(
         name='rvizconfig',
         default_value=os.path.join(my_robot_dir,'config','view.rviz'),
@@ -19,11 +26,13 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         output='screen',
+        parameters=[{'use_sim_time' : use_sim_time}],
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )  
 
     # Run the nodes
     return LaunchDescription([
+        declare_use_sim_time_cmd,
         rviz_arg,
         rviz2,
     ])
